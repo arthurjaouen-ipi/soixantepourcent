ad0f76704ac94abbafc10220da9dc0db0424b6a8
arthurjaouen-ipi <44089031+arthurjaouen-ipi@users.noreply.github.com>
2019-06-14 11:22:05 +0200

    Update front

--- public/js/accueil.js
+++ public/js/accueil.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	//getAllStories();
+	// getAllStories();
+
 	$(".story").hover(function(){
 		var parent = $(this).parent();
 		$(".story-text", parent).css("display","block");
@@ -12,18 +13,18 @@ $(document).ready(function(){
 
 function getAllStories(){
 	$.ajax({
-		method: "GET",
-		url: "/get/allstories",
+		type: "GET",
+		url: "",
 		success: function(data){
 			for(var key in data){
 				$(".stories-list").append("<div id='" + data[key][idStory] + "' class='col-lg-4 col-md-6 col-sm-12 story-container'><div class='story'></div><div class='story-text'></div></div>");
 				$(".stories-list #" + data[key][idStory] + " .story").css("background", data[key][imgStory] + "  no-repeat center fixed").css("background-size", "cover");
-				$(".stories-list #" + data[key][idStory] + " .story-text").text(data[key][txtStory]);
+				$(".stories-list #" + data[key][idStory] + " .story-text").text(data[key][txtStory] + "</br><button class='btn story-read'>Lire</button>");
 			}
 		},
 		error: function(){
 			alert("Erreur au chargement des histoires.");
-			throw new error("Error loading stories.");
+			throw new Error("Error loading stories.");
 		}
 	});
 }
