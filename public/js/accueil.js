933f7fa3421ce9c269e04d8dd118082b331c3c0d
arthurjaouen-ipi <44089031+arthurjaouen-ipi@users.noreply.github.com>
2019-06-14 12:21:47 +0200

    Update front

--- public/js/accueil.js
+++ public/js/accueil.js
@@ -9,6 +9,10 @@ $(document).ready(function(){
 	$(".story-text").mouseleave(function(){
 		$(this).css("display","none");
 	});
+
+	$(".story").each(function(){
+		$(this).css('background','url(img/img-foret.jpg');
+	});
 });
 
 function getAllStories(){
@@ -18,7 +22,7 @@ function getAllStories(){
 		success: function(data){
 			for(var key in data){
 				$(".stories-list").append("<div id='" + data[key][idStory] + "' class='col-lg-4 col-md-6 col-sm-12 story-container'><div class='story'></div><div class='story-text'></div></div>");
-				$(".stories-list #" + data[key][idStory] + " .story").css("background", data[key][imgStory] + "  no-repeat center fixed").css("background-size", "cover");
+				$(".stories-list #" + data[key][idStory] + " .story").css("background", data[key][imgStory]);
 				$(".stories-list #" + data[key][idStory] + " .story-text").text(data[key][txtStory] + "</br><button class='btn story-read'>Lire</button>");
 			}
 		},
